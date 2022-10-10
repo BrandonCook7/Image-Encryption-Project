f5f03b34fe2ed68be4fd177b69a97bd5b250c490
Brandon Cook <54552002+BrandonCook7@users.noreply.github.com>
2022-10-10 16:28:55 -0700

    Created Initial structure

---
+++ aes.py
@@ -0,0 +1,15 @@
+import pandas as pd
+class AES():
+    def __init__(self, key):
+        #Todo: Add a way to pad the key
+        #Key must be exactly 128 bits or exactly 16 chars currently
+        self.key = key
+        self.block_size = 16
+        self.rounds = 10
+        ord()
+
+    def encrypt_aes(key):
+        s = pd.Series([1,2])
+        print(s)
+        return
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from aes import *
+
+def main():
+    print("Hello World!")
+    encrypt_aes()
+
+if __name__ == "__main__":
+    main()
