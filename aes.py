8da270ba8eff9e14ed7000182c9eb22ef796321d
Brandon Cook <brandoncook7000@gmail.com>
2022-10-11 15:16:07 -0700

    Implemented a remove padding function that also checks if it was ever padded

--- aes.py
+++ aes.py
@@ -9,9 +9,14 @@ class AES():
         self.block_size = 128
         self.rounds = 10
 
-    def encrypt_aes(key):
-        s = pd.Series([1,2])
-        print(s)
+    def encrypt_aes(self):
+        #s = pd.Series([1,2])
+        hex_list = [format(ord(c), "x") for c in list(self.key_phrase) if True]
+        print("BEFORE PADDING: \n", hex_list)
+        self.pad_hex(hex_list)
+        print("AFTER PADDING: \n", hex_list)
+        self.unpad_hex(hex_list)
+        print("REMOVED PADDING: \n", hex_list)
         return
     def convert_key(self):
         #Example passphrase "password12345678"
@@ -26,10 +31,9 @@ class AES():
             hex_list = [format(ord(c), "x") for c in list(self.key_phrase) if True]
             m = np.array(hex_list).reshape(4,4).swapaxes(0,1)
             print(m)
-    #Uses PKCS#7 Padding
-    def pad_hex(self):
+    #Uses PKCS#7 Padding, modifies hex_list by reference
+    def pad_hex(self, hex_list: list):
         #Once hex value here is 8 bits/1 byte
-        hex_list = [format(ord(c), "x") for c in list(self.key_phrase) if True]
         blocks = 1
         bits_left = 0
         bits_total = len(hex_list) * 8
@@ -47,7 +51,21 @@ class AES():
         print(bytes_left)
         #For the PKCS#7 Padding you add the bytes left as a hex value to all the empty spaces
         hex_to_append = hex(bytes_left)
-        print("BEFORE PADDING: \n", hex_list)
         for i in range(bytes_left):
             hex_list.append(hex_to_append)
-        print("AFTER PADDING: \n", hex_list)
+    #Undoes PKCS#7 padding, modifies hex_list by reference
+    def unpad_hex(self, hex_list: list):
+        hex_len = hex_list[len(hex_list)-1]
+        padding_len = int(hex_len, 16)
+        rev_list = hex_list.copy()
+        rev_list.reverse()
+        #Check to see if hex list is padded
+        is_padded = True
+        for i in range(1, padding_len):
+            if rev_list[i] != rev_list[i-1]:
+                is_padded = False
+                break
+        if is_padded:
+            for i in range(padding_len):
+                hex_list.pop()
+        
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def main():
     #aes = AES("A quick brown fox jumps over dog")
     #aes = AES("A quick brown fox jumps over dogs")
     aes = AES("A quick brown fox jumps over dog, testing")
-    #aes.encrypt_aes()
+    aes.encrypt_aes()
     #aes.convert_key()
-    aes.pad_hex()
+    #aes.pad_hex()
 
 if __name__ == "__main__":
     main()
