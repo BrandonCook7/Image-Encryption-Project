a26a53c6cea492834df39c98ce3bc3c3b36bde20
Brandon Cook <brandoncook7000@gmail.com>
2022-10-13 00:23:52 -0700

    Build function that takes hex value and returns equivilant value from Rijndael S-Box

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from aes import *
 def main():
     print("Hello World!")
     #aes = AES("password12345678")
+    aes = AES("pa")
     #aes = AES("A quick brown fox jumps over dog")
     #aes = AES("A quick brown fox jumps over dogs")
-    aes = AES("A quick brown fox jumps over dog, testing")
-    aes.encrypt_aes()
-    #aes.convert_key()
+    #aes = AES("A quick brown fox jumps over dog, testing")
+    #aes.encrypt_aes()
+    aes.convert_key()
     #aes.pad_hex()
 
 if __name__ == "__main__":
